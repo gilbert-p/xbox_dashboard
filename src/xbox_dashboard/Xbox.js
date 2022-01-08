68e39094741eefef96cbcda127ae2f42431d5ce4
Gilberto Placidon <gilbertoplacidon@gmail.com>
2022-01-07 20:28:13 -0800

    Basic Ui (large screens) for xbox live content completed.

--- src/xbox_dashboard/Xbox.js
+++ src/xbox_dashboard/Xbox.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Dashboard.module.css';
+import ringAnim from './ringAnimation.module.css';
 import { navigateTo, selectCurrentContext } from './xboxSlice';
 
 const Xbox = () => {
@@ -20,19 +21,45 @@ const Xbox = () => {
                     <div className={styles.blade} onClick={()=> dispatch(navigateTo("media"))}><p>media</p></div>
                     <div className={styles.blade} onClick={()=> dispatch(navigateTo("system"))}><p>system</p></div>
                 </div>
+                <h2 className={styles.sectionHeading}>Xbox LIVE</h2>
                 <section className={styles.gamesContainer}>
-                    <div className={styles.whiteRadial}></div>
+
+                    {/* These elements are consistent across all contexts */}
                     <div className={styles.topBorder}></div>
-                    <h2 className={styles.sectionHeading}>Games</h2>
+                    <div className={styles.bottomBorder}></div>
+                    <div className={styles.buttonContainer}>
+                        <div className={`${styles.buttons}`}>
+                            <div id={styles["yButton"]} className={styles.buttonStyle}></div>
+                            <div id={styles["xButton"]} className={styles.buttonStyle}></div>
+                        </div>
+                        <div className={`${styles.buttons}`}>
+                            <div id={styles["bButton"]} className={styles.buttonStyle}></div>
+                            <div id={styles["aButton"]} className={styles.buttonStyle}></div>
+                        </div>
+                    </div>
+                    {/* These elements are consistent across all contexts */}
+
+
                     <div className={styles.contentContainer}>
                         <div className={styles.leftContent}>
                             <div className={styles.profileContainer}>
-                                <p>Sign In</p>
+                                <p>Epoxi117</p>
                                 <div className={styles.profileImgContainer}>
                                     <div className={styles.profileIcon}></div>
                                 </div>
                                 <div className={styles.profileDescription}>
-                                    <p>2 Profiles Found</p>
+                                    <p className={styles.repTitle}>Rep</p>
+                                    <div className={styles.reputationStars}>
+                                        <div className={styles.starIcon}></div>
+                                        <div className={styles.starIcon}></div>
+                                        <div className={styles.starIcon}></div>
+                                        <div className={styles.starIcon}></div>
+                                        <div className={styles.starIcon}></div>
+                                    </div>
+                                    <p className={styles.gamerscoreTitle}>Gamerscore</p>
+                                    <p className={styles.gamerscoreValue}>21117</p>
+                                    <p className={styles.zoneTitle}>Zone</p>
+                                    <div className={styles.zoneStatus}>Pro</div>
                                 </div>
                             </div>
                             <div className={styles.selectItemListContainer}>
@@ -42,7 +69,44 @@ const Xbox = () => {
                                 </div>
                             </div>
                             <div className={styles.xboxliveAnimationContainer}>
-                                <div className={styles.circleAnimation}></div>
+                                <div className={styles.circleAnimation}>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 0}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 1}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 2}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 3}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 4}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 5}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 6}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 7}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 8}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 9}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 10}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                    <div className={`${ringAnim.ring}`} style={{"--i": 11}}>
+                                        <div className={ringAnim.ringInner}></div>
+                                    </div>
+                                </div>
                                 <div className={styles.logoContainer}></div>
                             </div>
                         </div>
