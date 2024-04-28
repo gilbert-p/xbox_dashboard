5f6088384b0b5383097100dbede63300d0ea5f6e
Gilberto Placidon <gilbertoplacidon@gmail.com>
2024-04-28 15:32:50 -0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const cors = require('cors');
-
+const serverless = require('serverless-http');
 const db = require('../database/db');
 
 
